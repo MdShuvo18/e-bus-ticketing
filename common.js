75e4b6c7de458dbe5ebf0c3d4d77d7bf8deb9349
MdShuvo18 <mdks33881@gmail.com>
2024-02-18 22:37:44 +0600

    sit increment and decrement function done

---
+++ common.js
@@ -0,0 +1,4 @@
+// setInnerText function
+function setInnerText(id,value){
+    document.getElementById(id).innerText = value;
+ }
--- scripts.js
+++ scripts.js
@@ -1,6 +1,18 @@
 const sits = document.getElementsByClassName('sit')
+let sitLeft = 40;
+let sitAdd = 0;
+
 for (const sit of sits) {
-    sit.addEventListener('click', (e) => {
-        e.target.classList.toggle('active')
-    })
+   sit.addEventListener('click', function (e) {
+      // sit decrerment
+      sitLeft = sitLeft - 1;
+      setInnerText('sit-left',sitLeft)
+      // sit increment
+      sitAdd = sitAdd + 1;
+      setInnerText('sit-add',sitAdd)
+      
+      // set sitinnerText
+      const sitInnerText=e.target.innerText;
+      console.log(sitInnerText)
+      
 }
